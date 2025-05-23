e69f7b4d14e84854b4707d0e817abf2360974994
Marko <markomalevic@gmail.com>
2025-05-23 02:42:41 +0200

    registration, hashed password, connecting things

--- server/server.js
+++ server/server.js
@@ -3,23 +3,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config({ path: './server/.env' });
 console.log('Using MONGO_URI:', process.env.MONGO_URI);
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+.connect(process.env.MONGO_URI)
 .then(() => console.log('da konacno radi'))
 .catch((err) => console.error(' MongoDB works sweety', err));
 
 
+const authRoutes = require("./routes/authRoutes");
 
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
